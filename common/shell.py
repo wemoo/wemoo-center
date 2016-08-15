3298c4c4894634fff1ae95da7fa35bfd34351956
Shawn Tien <shawn@Shawns-MBP.lan>
2016-08-15 18:19:25 +0800

    add common/shell.exec

---
+++ common/shell.py
@@ -0,0 +1,10 @@
+from subprocess import STDOUT
+from subprocess import check_output
+from subprocess import TimeoutExpired
+
+def exec(cmd, timeout=5):
+    try:
+        output = check_output(cmd, stderr=STDOUT, timeout=timeout, shell=True)
+        return output
+    except TimeoutExpired:
+        return False
