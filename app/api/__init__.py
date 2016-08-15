3aabcc9a5578ffe96419ce514e64ca497c971de1
Shawn Tien <shawn@Shawns-MBP.lan>
2016-08-15 18:00:53 +0800

    add web hook api

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,8 +2,9 @@ from flask import Blueprint
 
 api = Blueprint('api', __name__)
 
-from . import logs  # NOQA
-from . import tasks  # NOQA
-from . import hosts  # NOQA
+from . import logs    # NOQA
+from . import tasks    # NOQA
+from . import hosts    # NOQA
+from . import webhook    # NOQA
 
-__all__ = [logs,  tasks, hosts]
+__all__ = [logs, tasks, hosts, webhook]
--- app/api/logs.py
+++ app/api/logs.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 import simplejson
 
 from flask import request
