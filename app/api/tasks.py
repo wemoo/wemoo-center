174635a13097eade8b072574eb1cc2b1a67c1256
Shawn Tien <shawn@Shawns-MBP.lan>
2016-08-19 11:50:11 +0800

    update task.py

--- app/api/tasks.py
+++ app/api/tasks.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
-
-import simplejson
+import datetime
 
 from flask import request
+from bson.json_util import dumps
+from bson.json_util import loads
+
 from common import render
 from app.api import api
 from app.models.task import Task
@@ -10,8 +11,8 @@ from app.models.task import Task
 
 @api.route('/tasks', methods=['GET'])
 def tasks_index():
-    tasks = Task.objects().to_json()
-    return render.ok({'tasks': tasks})
+    res = {'tasks': Task.objects(), 'time': datetime.datetime.now()}
+    return render.ok(res)
 
 
 @api.route('/tasks/<string:uuid>', methods=['GET'])
@@ -22,7 +23,8 @@ def tasks_get_via_uuid(uuid):
 
 @api.route('/tasks', methods=['PATCH'])
 def tasks_post():
-    data = simplejson.loads(request.data)
+    data = loads(request.data)
+    task = Task.objects(uuid=uuid)
     print(data)
 
     return render.ok()
