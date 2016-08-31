d8fe5fc610619b791309dae4e6201388ba9b6fa7
Shawn Tien <hightian@gmail.com>
2016-08-31 13:53:26 +0800

    add to_dict

--- app/models/task.py
+++ app/models/task.py
@@ -8,13 +8,26 @@ db = environment.mdb
 
 
 class Task(db.Document):
+    TYPE_ONCE = 1
+    TYPE_CONTINUES = 2
 
     title = db.StringField(required=True, max_length=100)
-    uuid = db.StringField(max_length=100)
+    task_type = db.IntField(required=True)
     desc = db.StringField(required=True, max_length=1000)
     script = db.StringField(required=True)
-    result = db.StringField(required=True)
+    result = db.StringField(default=None)
     finished = db.BooleanField(default=False)
 
     created_at = db.DateTimeField(default=datetime.datetime.now)
     updated_at = db.DateTimeField(default=datetime.datetime.now)
+
+    def to_dict(self):
+        return {
+            'title': self.title,
+            'task_type': self.task_type,
+            'desc': self.desc,
+            'script': self.script,
+            'result': self.result,
+            'created_at': str(self.created_at),
+            'updated_at': str(self.updated_at)
+        }
