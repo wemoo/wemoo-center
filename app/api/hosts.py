2658ff21d8a5401f817e4cb86de4eab560071f7d
Shawn Tien <hightian@gmail.com>
2016-09-03 13:11:07 +0800

    update hosts

--- app/api/hosts.py
+++ app/api/hosts.py
@@ -19,10 +19,9 @@ def hosts_post():
         return render.error()
 
     now = datetime.datetime.now()
-    result = Host.objects(uuid=uuid).update(set__hostname=hostname,
-                                            set_on_insert__created_at=now,
-                                            updated_at=now,
-                                            upsert=True)
+    result = Host.objects(uuid=uuid).update(
+        set__hostname=hostname, set_on_insert__created_at=now, updated_at=now, upsert=True)
+
     if result > 0:
         return render.ok()
     else:
