de370dbe6d5eb775aba86cdb30ecfc1bef888ac7
Shawn Tien <hightian@gmail.com>
2016-09-03 17:13:32 +0800

    refactor structure of Host

--- app/models/host.py
+++ app/models/host.py
@@ -9,9 +9,14 @@ db = environment.mdb
 
 class Host(db.Document):
 
-    uuid = db.StringField(required=True)
+    uuid = db.StringField(required=True, unique=True)
+    system = db.StringField(required=True)
+    hostname = db.StringField(required=True)
+    release = db.StringField(required=True)
+    version = db.StringField(required=True)
+    machine = db.StringField(required=True)
+    processor = db.StringField(required=True)
     ip = db.DictField()
-    hostname = db.StringField(required=True, max_length=200)
 
     created_at = db.DateTimeField(default=datetime.datetime.now)
     updated_at = db.DateTimeField(default=datetime.datetime.now)
