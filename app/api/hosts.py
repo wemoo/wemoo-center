7924c04bf22a2c518fe56a4a7360ad3d25b0107d
Shawn Tien <hightian@gmail.com>
2016-09-03 17:13:58 +0800

    update API POST /api/hosts

--- app/api/hosts.py
+++ app/api/hosts.py
@@ -12,15 +12,18 @@ from app.models.host import Host
 @api.route('/hosts', methods=['POST'])
 def hosts_post():
     data = simplejson.loads(request.data)
-    uuid = data.get('uuid', None)
-    hostname = data.get('hostname', None)
-
-    if not (uuid and hostname):
-        return render.error()
 
     now = datetime.datetime.now()
-    result = Host.objects(uuid=uuid).update(
-        set__hostname=hostname, set_on_insert__created_at=now, updated_at=now, upsert=True)
+    result = Host.objects(uuid=data['uuid']).update(
+        set__system=data['system'],
+        set__hostname=data['node'],
+        set__release=data['release'],
+        set__version=data['version'],
+        set__machine=data['machine'],
+        set__processor=data['processor'],
+        set_on_insert__created_at=now,
+        updated_at=now,
+        upsert=True)
 
     if result > 0:
         return render.ok()
